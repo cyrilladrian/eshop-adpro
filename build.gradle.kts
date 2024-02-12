eaf7fb284bcb60e9e938c8e29868bdfb194195d2
cyrilladrian <cyrilladrian.wicaksono@gmail.com>
2024-02-12 22:57:46 +0700

    update gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     java
+    jacoco
     id("org.springframework.boot") version "3.2.2"
     id("io.spring.dependency-management") version "1.1.4"
 }
@@ -61,5 +62,15 @@ tasks.withType<Test>().configureEach {
     useJUnitPlatform()
 }
 
+tasks.test{
+    filter {
+        excludeTestsMatching("*FunctionalTest")
+    }
+
+    finalizedBy(tasks.jacocoTestReport)
+}
+tasks.jacocoTestReport{
+    dependsOn(tasks.test)
+}
 
 
